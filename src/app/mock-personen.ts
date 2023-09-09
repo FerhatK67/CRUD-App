a2bd7b0527ed02f2144ec3d7510b0811d4fe4d64
Ferhat Kuluclu <ferhat.kuluclu@stud.uni-due.de>
2023-09-09 12:27:21 +0200

    personen array in eine seperate file verlegt

---
+++ src/app/mock-personen.ts
@@ -0,0 +1,11 @@
+import { Person } from "./person";
+
+  export const PERSONEN: Person[] = [
+      {id: 1, vorname: "Jon", nachname: "Snow", email: "jon@mail.com"},
+      {id: 2, vorname: "Daenerys", nachname: "Targaryen", email: "daenerys@mail.com"},
+      {id: 3, vorname: "Tyrion", nachname: "Lannister", email: "tyrion@mail.com"},
+      {id: 4, vorname: "Arya", nachname: "Stark", email: "arya@mail.com"},
+      {id: 5, vorname: "Cersei", nachname: "Lannister", email: "cersei@mail.com"},
+      {id: 6, vorname: "Jaime", nachname: "Lannister", email: "jaime@mail.com"},
+      {id: 7, vorname: "Sansa", nachname: "Stark", email: "sansa@mail.com"}
+  ];
